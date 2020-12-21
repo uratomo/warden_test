c2cef389d5dc1b354564b074a092a14d339cf51f
uratomo <rop.ibuki@gmail.com>
2020-12-21 23:59:12 +0900

    top_controller create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root "top#index"
+
+  get 'top/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
